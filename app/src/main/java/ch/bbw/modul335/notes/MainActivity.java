e16413b8e400b92c667f7cd906e87c388cd48503
ayu-ahilan <ahilayu@hotmail.com>
2021-12-08 22:48:30 +0100

    background color and recyclerview put on correct place

--- app/src/main/java/ch/bbw/modul335/notes/MainActivity.java
+++ app/src/main/java/ch/bbw/modul335/notes/MainActivity.java
@@ -1,14 +1,22 @@
 package ch.bbw.modul335.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.listOfNotes);
     }
 }
