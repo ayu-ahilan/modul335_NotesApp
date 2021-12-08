3f6a3967f329022456702ab2aec21e17d600327f
ayu-ahilan <ahilayu@hotmail.com>
2021-12-08 22:57:51 +0100

    Toolbar in AddNote

--- app/src/main/java/ch/bbw/modul335/notes/AddNote.java
+++ app/src/main/java/ch/bbw/modul335/notes/AddNote.java
@@ -1,14 +1,18 @@
 package ch.bbw.modul335.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class AddNote extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
